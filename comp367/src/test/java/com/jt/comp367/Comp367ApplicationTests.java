0b260826b08b3b82a3a3a4aaf83b64e5b07a668e
Jason Tse <59128576+jasonjytse@users.noreply.github.com>
2023-10-30 19:40:11 -0400

    Welcome to COMP367

---
+++ comp367/src/test/java/com/jt/comp367/Comp367ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jt.comp367;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Comp367ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
